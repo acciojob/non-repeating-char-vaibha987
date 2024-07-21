1460a5ed67450540dae39cdfc61f6520ec1120aa
vaibha987 <v2goyal95@gmail.com>
2024-07-21 17:18:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 function firstNonRepeatedChar(str) {
- // Write your code here
+    let charCount = {};
+    for(let i = 0; i < str.length; i++){
+        if(charCount.hasOwnProperty(str[i])){
+            charCount[str[i]]++;
+        } else {
+            charCount[str[i]] = 1;
+        }
+    }
+
+    for(let i = 0; i < str.length; i++){
+        if(charCount[str[i]] == 1){
+            return str[i];
+        }
+    }
+
+    return null;
 }
-const input = prompt("Enter a string");
-alert(firstNonRepeatedChar(input)); 
+//const input = prompt("Enter a string");
+//alert(firstNonRepeatedChar(input)); 
